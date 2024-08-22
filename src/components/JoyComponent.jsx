35f73114ce11d4b752c211fb6a901861a6d4db24
Jasjappan SIngh <jasjap@gmail.com>
2024-08-22 17:09:42 +0530

    first commmit

---
+++ src/components/JoyComponent.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import { Joystick } from "react-joystick-component";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "600px",
+    margin: "35px 0",
+    padding: 0,
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
+    },
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+}));
+
+function JoyComponent() {
+  const classes = useStyles();
+  const [joystickData, setJoystickData] = useState({
+    surge: 0,
+    sway: 0,
+    heave: 0,
+    yaw: 0,
+  });
+
+  useEffect(() => {
+    sendDataRedis(joystickData);
+  }, [joystickData]);
+
+  //Joystick One
+  const handleMove = (joyOneData) => {
+    const newData = {
+      ...joystickData,
+      surge: joyOneData.y,
+      sway: joyOneData.x,
+    };
+    setJoystickData(newData);
+    // sendDataRedis(joystickData);
+  };
+  //Joystick Two
+  const handleMove2 = (joyTwoData) => {
+    const newData = {
+      ...joystickData,
+      heave: joyTwoData.y,
+      yaw: joyTwoData.x,
+    };
+    setJoystickData(newData);
+    // sendDataRedis(joystickData);
+  };
+  //Sending Joystick Data
+  const sendDataRedis = async (data) => {
+    try {
+      await axios.post("https://nio-server.vercel.app/joystick-data", data);
+      console.log("Data Sent");
+    } catch (error) {
+      console.log(error);
+      console.error("Error sending data to Redis:", error);
+    }
+  };
+  // const handleStop = () => {};
+  return (
+    <>
+      {/* <div className="gap-1 columns-2 flex felx-row  justify-between "> */}
+      <Container className={classes.container}>
+        <div>
+          <Joystick
+            baseColor="#355764"
+            stickColor="#FFEA11"
+            size={100}
+            move={(e) => {
+              handleMove({ x: e.x, y: e.y });
+            }}
+            stop={() => {
+              handleMove({ x: 0, y: 0 });
+            }}
+          />
+        </div>
+        <div>
+          <Joystick
+            baseColor="#355764"
+            stickColor="#FFEA11"
+            size={100}
+            move={(e) => {
+              handleMove2({ x: e.x, y: e.y });
+            }}
+            stop={() => {
+              handleMove2({ x: 0, y: 0 });
+            }}
+          />
+        </div>
+      </Container>
+    </>
+  );
+}
+export default JoyComponent;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useContext } from "react";
+import { Button, TextField, Grid, Container, Paper } from "@material-ui/core";
+// import { CopyToClipboard } from "react-copy-to-clipboard";
+import { PhoneDisabled } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { SocketContext } from "../Context";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  gridContainer: {
+    width: "100%",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
+  },
+  container: {
+    width: "600px",
+    margin: "35px 0",
+    padding: 0,
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
+    },
+  },
+  margin: {
+    marginTop: 20,
+  },
+  padding: {
+    padding: 20,
+  },
+  paper: {
+    padding: "10px 20px",
+    border: "2px solid black",
+  },
+}));
+
+const Sidebar = ({ children }) => {
+  const { callAccepted, callEnded, leaveCall, callUser } =
+    useContext(SocketContext);
+  const [idToCall, setIdToCall] = useState("");
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <TextField
+                label="Enter ID"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                fullWidth
+              />
+              {callAccepted && !callEnded ? (
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<PhoneDisabled fontSize="large" />}
+                  fullWidth
+                  onClick={leaveCall}
+                  className={classes.margin}
+                >
+                  Hang Up
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={() => callUser(idToCall)}
+                  className={classes.margin}
+                >
+                  Connect
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+        {children}
+      </Paper>
+    </Container>
+  );
+};
+
+export default Sidebar;
